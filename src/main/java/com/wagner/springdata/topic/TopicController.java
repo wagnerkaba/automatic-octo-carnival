2226f0db23d46db7578f65e5fba6237d3b71b67c
wagnerkaba <bom.link+github@gmail.com>
2021-11-25 13:50:50 -0300

    Primeiro commit
    https://www.youtube.com/watch?v=du99WcQztNU&list=PLqq-6Pq4lTTbx8p2oCgcAQGQyqN8XeA1x&index=27

---
+++ src/main/java/com/wagner/springdata/topic/TopicController.java
@@ -0,0 +1,67 @@
+package com.wagner.springdata.topic;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class TopicController {
+
+    //Explicação by WK:
+    //Como a classe TopicService está anotada como @Service, a anotação
+    //@Autowired abaixo faz com que o haja injeção de dependência em topicService
+    //
+    // @Autowired allows Spring to resolve and inject collaborating beans into our bean.
+    @Autowired
+    private TopicService topicService;
+
+    @RequestMapping("/topicsraw")
+    public List<Topic> getTopicsRaw(){
+
+        //Se http://localhost:8080/topicsra for acessado, ele vai mostrar
+        //os objetos abaixo convertidos em JSON
+
+        return Arrays.asList(
+                new Topic("Spring", "Spring Framework", "description"),
+                new Topic("java", "Core java", "description"),
+                new Topic("javascript", "javascript", "description")
+        );
+    }
+
+    @RequestMapping("/topics")
+    public List<Topic> getAllTopics(){
+        return topicService.getAllTopics();
+    }
+
+    // Colocar id entre chaves ({id}) diz ao spring que se trata de uma variavel
+    @RequestMapping("/topics/{id}")
+    // @PathVariable diz ao spring que o parâmetro id = {id}
+    public Topic getTopic(@PathVariable String id){
+        return topicService.getTopic(id);
+
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/topics")
+    public void addTopic(@RequestBody Topic topic){
+
+        topicService.addTopíc(topic);
+
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/topics/{id}")
+    public void updateTopic(@RequestBody Topic topic, @PathVariable String id){
+
+        topicService.updateTopíc(id, topic);
+
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/topics/{id}")
+    public void deleteTopic(@PathVariable String id){
+
+        topicService.deleteTopíc(id);
+
+    }
+
+}
---
+++ src/main/java/com/wagner/springdata/topic/TopicService.java
@@ -0,0 +1,51 @@
+package com.wagner.springdata.topic;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class TopicService {
+
+
+
+    private List<Topic> topics = new ArrayList<>(
+            Arrays.asList(
+                    new Topic("Spring", "Spring Framework", "description"),
+                    new Topic("java", "Core java", "description"),
+                    new Topic("javascript", "javascript", "description")
+            )
+    );
+
+    public List<Topic> getAllTopics() {
+        return topics;
+    }
+
+    public Topic getTopic(String id){
+        return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+    }
+
+
+    public void addTopíc(Topic topic) {
+
+        topics.add(topic);
+
+    }
+
+
+    public void updateTopíc(String id, Topic topic) {
+        for (int i =0; i < topics.size(); i++){
+            Topic t = topics.get(i);
+            if (t.getId().equals(id)) {
+                topics.set(i, topic);
+                return;
+            }
+        }
+    }
+
+    public void deleteTopíc(String id) {
+        topics.removeIf(t -> t.getId().equals(id));
+    }
+}
