ad4ee450699448b1ce7a90cb7231d1ae253f99e0
wagnerkaba <bom.link+github@gmail.com>
2021-11-26 11:41:10 -0300

    Implementação das operações CRUD utilizando Repository
    https://www.youtube.com/watch?v=lpcOSXWPXTk&list=PLqq-6Pq4lTTbx8p2oCgcAQGQyqN8XeA1x&index=30

--- src/main/java/com/wagner/springdata/topic/Topic.java
+++ src/main/java/com/wagner/springdata/topic/Topic.java
@@ -1,6 +1,12 @@
 package com.wagner.springdata.topic;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Topic {
+
+    @Id
     private String id;
     private String name;
     private String description;
